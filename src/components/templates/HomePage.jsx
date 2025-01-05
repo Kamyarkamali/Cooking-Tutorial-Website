62784ab460f47b9a85c275552c80f5e33be9a153
kamyarkamali <kamyarkamali021@gmail.com>
2025-01-05 07:15:32 +0330

    first commit

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -2,7 +2,6 @@ import HeroPage from "../module/HeroPage";
 import HowCook from "../module/HowCook";
 import Toutorial from "../module/toturial1";
 import Toutorial2 from "../module/Toturial2";
-import SugestFood from "./SugestFood";
 
 function HomePage() {
   return (
